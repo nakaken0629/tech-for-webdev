eb4589063494617476c4a2309b6c7d13bfe38289
NAKAGAKI Kenji <nakaken0629@gmail.com>
2023-09-30 22:45:01 +0900

    Java環境を構築

---
+++ ch9/java/dev/work/Hello.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("こんにちは世界 ");
+        Date today = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("今日は y-M-d");
+        System.out.println(format.format(today));
+    }
+}
+    
