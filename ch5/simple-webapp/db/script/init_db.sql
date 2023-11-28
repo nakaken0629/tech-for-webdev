fff4736c72543718e22a49536d139666db11fffd
NAKAGAKI Kenji <nakaken0629@gmail.com>
2023-11-28 09:55:49 +0900

    データベースで文字コードを指定

--- ch5/simple-webapp/db/script/init_db.sql
+++ ch5/simple-webapp/db/script/init_db.sql
@@ -1,6 +1,12 @@
+-- simplewebデータベースを作成する
+create database simpleweb;
+use simpleweb;
+
+-- user1ユーザーを作成する
 create user 'user1'@'%' identified by 'password1234';
 grant all on *.* to 'user1'@'%';
 
+-- postテーブルを作成する
 create table post (
     id MEDIUMINT NOT NULL AUTO_INCREMENT,
     posted_at datetime,
