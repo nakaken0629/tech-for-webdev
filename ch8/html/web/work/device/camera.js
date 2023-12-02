9907b70bc246a368d22ec7f5a4efd951085de2e7
NAKAGAKI Kenji <nakaken0629@gmail.com>
2023-12-02 23:14:54 +0900

    scriptにdeferをつけて実行タイミングをコントロールする

--- ch8/html/web/work/device/camera.js
+++ ch8/html/web/work/device/camera.js
@@ -1,14 +1,12 @@
-window.addEventListener('DOMContentLoaded', () => {
-    if (typeof navigator.mediaDevices.getUserMedia !== 'function') {
-        const err = new Error('カメラが使えません');
-        alert(`${err.name} ${err.message}`);
-        throw err;
-    }
-    const $start = document.getElementById('start_btn');
-    const $video = document.getElementById('video_area');
-    $start.addEventListener('click', () => {
-        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-            .then(stream => $video.srcObject = stream)
-            .catch(err => alert(`${err.name} ${err.message}`));
-    }, false);
-});
+if (typeof navigator.mediaDevices.getUserMedia !== 'function') {
+    const err = new Error('カメラが使えません');
+    alert(`${err.name} ${err.message}`);
+    throw err;
+}
+const $start = document.getElementById('start_btn');
+const $video = document.getElementById('video_area');
+$start.addEventListener('click', () => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(stream => $video.srcObject = stream)
+        .catch(err => alert(`${err.name} ${err.message}`));
+}, false);
