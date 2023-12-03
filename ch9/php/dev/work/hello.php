d135bf41615fa57993dbe200c288bfb512583cb3
NAKAGAKI Kenji <nakaken0629@gmail.com>
2023-12-03 09:44:01 +0900

    文字コードを追加

--- ch9/php/dev/work/hello.php
+++ ch9/php/dev/work/hello.php
@@ -1,3 +1,11 @@
-<?php
-echo "こんにちは、世界\n";
-echo "今日は " . date("Y-m-d", strtotime("today")) . "\n"; 
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+<body>
+  <?php
+  echo "こんにちは、世界<br>\n";
+  echo "今日は " . date("Y-m-d", strtotime("today")) . "<br>\n";
+  ?>
+</body>
+</html>
