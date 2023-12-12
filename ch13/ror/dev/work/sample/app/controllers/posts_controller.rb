6cfa058703c61926245e4342d909cdf4417b6b9b
NAKAGAKI Kenji <nakaken0629@gmail.com>
2023-12-13 02:37:38 +0900

    databaseをMySQLにして再作成

--- ch13/ror/dev/work/sample/app/controllers/posts_controller.rb
+++ ch13/ror/dev/work/sample/app/controllers/posts_controller.rb
@@ -1,13 +1,16 @@
 class PostsController < ApplicationController
   def index
+    # postsテーブルから全件取得する
     @posts = Post.all()
   end
 
   def create
+    # postsテーブルに登録する
     post = Post.new(post_params)
     post.posted_at = DateTime.now
     post.save()
 
+    # postsテーブルから全件取得する
     @posts = Post.all()
     render "posts/index"
   end
--- ch13/ror/dev/work/sample/test/controllers/posts_controller_test.rb
+++ ch13/ror/dev/work/sample/test/controllers/posts_controller_test.rb
@@ -5,4 +5,9 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     get posts_index_url
     assert_response :success
   end
+
+  test "should get create" do
+    get posts_create_url
+    assert_response :success
+  end
 end
