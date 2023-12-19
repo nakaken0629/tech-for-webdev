81bf0481642c5098d026078b135203dbe997d8a0
NAKAGAKI Kenji <nakaken0629@gmail.com>
2023-12-20 04:24:34 +0900

    Expressプロジェクトの新規作成

--- ch13/express/dev/work/simpleweb/create_table.sql
+++
@@ -1,6 +0,0 @@
-create table post (
-    id MEDIUMINT NOT NULL AUTO_INCREMENT,
-    posted_at datetime,
-    article varchar(1000),
-    primary key(id)
-)
--- ch13/express/dev/work/simpleweb/routes/index.js
+++ ch13/express/dev/work/simpleweb/routes/index.js
@@ -1,32 +1,9 @@
-const mysql = require('mysql2/promise');
-const createConnection = require('../db.js')
-
 var express = require('express');
 var router = express.Router();
 
-const readPosts = async(connection) => {
-  const sql = "select posted_at, article from post";
-  const [posts, fields] = await connection.execute(sql);
-  return posts;
-}
-
 /* GET home page. */
-router.get('/', async (req, res, next) => {
-  const connection = await createConnection();
-  const posts = await readPosts(connection);
-  connection.close();
-  res.render('index', { posts: posts });
-});
-
-/* GET home page. */
-router.post('/', async (req, res, next) => {
-  const connection = await createConnection();
-  article = req.body.article;
-  const sql = "insert into post (posted_at, article) values (now(), ?)";
-  connection.execute(sql, [article]);
-  const posts = await readPosts(connection);
-  connection.close();
-  res.render('index', { posts: posts });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
