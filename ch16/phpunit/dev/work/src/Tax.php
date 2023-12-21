0dafeae5b711310a5f7d0468c22e638c92c73def
NAKAGAKI Kenji <nakaken0629@gmail.com>
2023-12-21 20:16:56 +0900

    phpunit.xmlを簡素化

--- ch16/phpunit/dev/work/src/Tax.php
+++ ch16/phpunit/dev/work/src/Tax.php
@@ -1,9 +1,22 @@
 <?php
+
 namespace Root\Root;
+
 final class Tax
 {
     public function calc_tax(int $price, bool $is_exempted, bool $is_reduced, bool $is_taken_out): int
     {
-        return 0;
+        if ($is_exempted)
+        {
+            return $price;
+        }
+        else if ($is_reduced && $is_taken_out)
+        {
+            return (int) ($price * 1.08);
+        }
+        else
+        {
+            return (int) ($price * 1.1);
+        }
     }
 }
