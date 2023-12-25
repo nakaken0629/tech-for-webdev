81aebf11bd83f558855f46bc662d02f1818ce620
NAKAGAKI Kenji <nakaken0629@gmail.com>
2023-12-25 16:26:07 +0900

    商品を検索するシナリオを作成

--- ch16/selenium/dev/work/sample/src/App.vue
+++ ch16/selenium/dev/work/sample/src/App.vue
@@ -2,13 +2,15 @@
   <div>
     <h1>商品一覧</h1>
     <p>
-      <button @click="fetchProductItems">商品検索</button>
+      <button id="search" @click="fetchProductItems">商品検索</button>
       テストコメント <input type="text" value="">
     </p>
   </div>
-  <ProductItem v-for="item in items" :key="item.id" :id="item.id" :name="item.name" :price="item.price"
-    :photo="item.photo">
-  </ProductItem>
+  <div id="products">
+    <ProductItem v-for="item in items" :key="item.id" :id="item.id" :name="item.name" :price="item.price"
+      :photo="item.photo">
+    </ProductItem>
+  </div>
 </template>
 
 <script setup>
---
+++ ch16/selenium/tester/work/show_products.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
+
+CURRENT_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# 画面を表示しないので、ヘッドレスオプションを付ける
+options = webdriver.FirefoxOptions()
+options.add_argument('--headless')
+options.add_argument('--window-size=1280,1024')
+
+with webdriver.Remote(
+    command_executor='http://driver-firefox:4444',
+    options=options,
+) as driver:
+    # 画面を開く
+    driver.get("http://dev:8080/")
+    driver.save_screenshot(os.path.join(CURRENT_DIR_PATH, "evidences", "1.png"))
+    products = driver.find_element(By.ID, "products")
+    assert 0 == len(products.find_elements(By.TAG_NAME, "div"))
+
+    # 商品を検索する
+    driver.find_element(By.ID, "search").click()
+    driver.save_screenshot(os.path.join(CURRENT_DIR_PATH, "evidences", "2.png"))
+    products = driver.find_element(By.ID, "products")
+    assert 3 == len(products.find_elements(By.TAG_NAME, "div"))
+    
